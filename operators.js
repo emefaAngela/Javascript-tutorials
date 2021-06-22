c091e6dadad64843b1981fad6f26c6d11ab9b75e
emefaAngela <63966516+emefaAngela@users.noreply.github.com>
2021-06-22 04:39:54 +0000

    assignment and arithmetiic operators in js

---
+++ operators.js
@@ -0,0 +1,30 @@
+ //operators are used for creating logical statements.
+   
+ //assignment operators
+
+ //"=" is used to assign a value to a variable
+ 
+ var num = 1;
+
+ //"+=" is used to add a value and assign the result to the variable on the left of the operator
+ num = num+ 5;
+ num += 5;
+
+ //Arithmetic operators
+
+ // "+" for addition operations
+ // "-" for subtraction operations
+// "*" for multiplication operations
+// "/" for division operations
+
+
+// STRING OPERATORS
+// used for combining strings with other types of data
+
+var age = 27;
+var info = "My name is Daniel, I am " + age +" years old";
+
+//string operators can be used to add space
+var number = 4 + "" + 5;
+
+
