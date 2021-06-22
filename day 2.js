4a252c60736f327feb98d1ada52428c4e97b5ea6
emefaAngela <63966516+emefaAngela@users.noreply.github.com>
2021-06-22 04:37:19 +0000

    rules in writing javascript codes

---
+++ day 2.js
@@ -0,0 +1,21 @@
+//This is a comment.
+
+/*
+This is a multiline comment.
+*/
+
+//writing an output to the screen in javascript
+document.body.innerHTML = " output string goes here";
+
+//javascript is case-sensitive
+
+//a constant variable(const) is named with all capital letters
+
+const NAME = "daniel";
+
+ //stings are written in double quotes
+ var a = "My name is angela";
+
+ //every javascript statement ends with a semicolon
+
+
